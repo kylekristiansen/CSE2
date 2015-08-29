cb633f3a353d18c6fb6652716c7bec6c87208967
kylekristiansen <kdk318@lehigh.edu>
2015-08-29 18:40:33 +0000

    hw01 updated

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+/////////////////////////////////////////////////////////////////////////
+//Kyle Kristiansen
+//First homework assignment
+//Welcome Class Java Program
+//
+// write the program
+// compile the program
+//    javac WelcomeClass.java
+// run the program
+//    java WelcomeClass
+
+// define the class (name)
+public class WelcomeClass{
+    
+// add main method
+    public static void main(String[] args) {
+    
+//   print the elaborate message using command System.out.println("...")
+     System.out.println("   -----------          ");
+     System.out.println("   | Welcome |          ");
+     System.out.println("   -----------          ");
+     System.out.println("   ^  ^  ^  ^  ^  ^     ");
+     System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\    ");
+     System.out.println(" <-K--D--K--3--1--8->   ");
+     System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /    ");
+     System.out.println("   v  v  v  v  v  v     ");
+     System.out.println("Over the summer I camped outside of the Dover Speedway for a 4 day music festival called Firefly. My favorite performer was The Killers");
+     
+     //test/
+    }
+}
